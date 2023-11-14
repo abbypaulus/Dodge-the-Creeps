510b9d5a62f738e2da1fa12dc644d42d94c72047
Abigail Paulus <abbypaulus@icloud.com>
2023-11-14 07:27:55 -0500

    Fixed bugs and added HUD

--- main.gd
+++ main.gd
@@ -2,47 +2,55 @@ extends Node
 
 @export var mob_scene: PackedScene
 var score
-# Called when the node enters the scene tree for the first time.
 
-func ready():
-	new_game()
-
+func _ready():
+	pass
+	
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
-	
-func  new_game():
+	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
+
+func new_game():
 	score = 0
+	get_tree().call_group("mobs", "queue_free")
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	$Music.play()
 
 func _on_mob_timer_timeout():
-	#create new instatnce of mob scene
+	# Create a new instanece of the Mob scene
 	var mob = mob_scene.instantiate()
 	
-	#choose random location on path2d
-	var mob_spawn_location = get_node('MobPath/MobSpawnLocation')
+	# Choose a random location on Path2D
+	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
 	mob_spawn_location.progress_ratio = randf()
 	
-	#set mobs direction perependicular to path
+	#set the mob's direction perpendicular to the path direction
 	var direction = mob_spawn_location.rotation + PI / 2
 	
-	#set mobs position to random location
+	# Set the mob's position to a random location
 	mob.position = mob_spawn_location.position
 	
-	#add randomness to direction
+	# Add some randomness to the direction
 	direction += randf_range(-PI / 4, PI / 4)
 	mob.rotation = direction
 	
-	#choose velocity for mob
-	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	# Choose the velocity for the mob
+	var velocity = Vector2(randf_range(150.0, 250.0), 0)
 	mob.linear_velocity = velocity.rotated(direction)
-	
-	#spawn mob by adding to main
+
+	# Spawn the mob by adding it to the main scene
 	add_child(mob)
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
+
 
 func _on_start_timer_timeout():
 	$MobTimer.start()
--- player.gd
+++ player.gd
@@ -1,15 +1,15 @@
 extends Area2D
 signal hit
 
-@export var speed = 400 #player speed px/sec
-var  screen_size #size of window
+@export var speed = 400 #Player speed in pixles per second
+var screen_size 
 
 func _ready():
 	screen_size = get_viewport_rect().size
 	hide()
-	
+
 func _process(delta):
-	var  velocity = Vector2.ZERO #movement vector
+	var velocity = Vector2.ZERO # The player's movement vector
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("move_left"):
@@ -22,26 +22,23 @@ func _process(delta):
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite2D.play()
-	else: 
+	else:
 		$AnimatedSprite2D.stop()
-		
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
-	
 	if velocity.x != 0:
-		$AnimatedSprite2D.animation = 'walk'
+		$AnimatedSprite2D.animation = "walk"
 		$AnimatedSprite2D.flip_v = false
 		$AnimatedSprite2D.flip_h = velocity.x < 0
 	elif velocity.y != 0:
-		$AnimatedSprite2D.animation = 'up'
+		$AnimatedSprite2D.animation = "up"
 		$AnimatedSprite2D.flip_v = velocity.y > 0
-		
 
 
 func _on_body_entered(body):
 	hide()
 	hit.emit()
-	$CollisionShape2D.set_deferred('disabled', true)
+	$CollisionShape2D.set_deferred("disabled", true)
 
 func start(pos):
 	position = pos
